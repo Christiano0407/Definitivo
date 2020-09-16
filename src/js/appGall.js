73c524ef70d49fcabdc260036e633b2b660322de
Christiano0407 <placebo0704@gmail.com>
2020-09-16 11:17:34 -0500

    close-img

--- src/js/appGall.js
+++ src/js/appGall.js
@@ -3,13 +3,20 @@ let getLatestOpenedImg;
 let windowWidth = window.innerWidth;
 
 if(galleryImages) {
-    galleryImages.forEach(function(image) {
+    galleryImages.forEach(function(image, index) {
         image.onclick = function() {
             let getElementCss = window.getComputedStyle(image);
             let getFullImgUrl = getElementCss.getPropertyValue("background-image");
             let getImgUrlPos = getFullImgUrl.split("/src/images/");
             let setNewImgUrl = getImgUrlPos[1].replace(`")`, ``);
-            alert(getImgUrlPos[1]);
+            /* alert(getImgUrlPos[1]); */
+            getLatestOpenedImg = index + 1;
+
+            let container = document.body;
+            let newImgWindow = document.createElement("div");
+            container.appendChild(newImgWindow);
+            newImgWindow.setAttribute("class", "img-window");
+            newImgWindow.setAttribute("onclick", "closeImg()");
         }
     });
 }
