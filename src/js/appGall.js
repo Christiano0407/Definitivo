698e1c507fcb2a9cef6f87b7057782a192f5b955
Christiano0407 <placebo0704@gmail.com>
2020-09-16 10:18:41 -0500

    gall-js

---
+++ src/js/appGall.js
@@ -0,0 +1,11 @@
+let galleryImages = document.querySelectorAll(".gallery-img");
+let getLatestOpenedImg;
+let windowWidth = window.innerWidth;
+
+if(galleryImages) {
+    galleryImages.forEach(function(image) {
+        image.onClick = function() {
+            let getElementCss = window.getComputedStyle();
+        }
+    });
+}
