922ad1d1922cd4c0e375d411c5617d2bd77bcdd4
Christiano0407 <placebo0704@gmail.com>
2020-09-22 00:00:29 -0500

    jav-1

---
+++ src/js/javApp.js
@@ -0,0 +1,24 @@
+/* var name = "Cader";
+
+console.log(name); */
+
+/* function logger() {
+    console.log("Party");
+    console.log("Party");
+    console.log("Party");
+    console.log("Party");
+}
+logger(); */
+
+/* const name = "cader";
+
+function toUpper(text) {
+const upperCased = text.toUpperCase();
+console.log(upperCased);
+};
+toUpper(name); */
+
+/* const toUpper = () => {
+    console.log("Hola");
+};
+toUpper(); */
