7b9534a3c4899e596a52459b6de01d368f379c3e
Christiano0407 <placebo0704@gmail.com>
2020-10-12 13:00:33 -0500

    mjss

---
+++ src3/js/gridApp.js
@@ -0,0 +1,19 @@
+/* const instagram = {
+    section: document.getElementById("instagram"),
+    link: document.getElementById("link-instagram")
+}
+
+var paginaActual = instgram;
+instagram.link.classList.add("link-activo");
+
+instagram.link.addEventListener("click", () => mostrarSeccion(instagram))
+
+function mostrarSeccion(paginaNueva) {
+    paginaActual.section.style.display = "none";
+    paginaNueva.section.style.display = "block";
+    
+    paginaActual.link.classList.remove("link-activo")
+    paginaNueva.link.classList.add("link-activo");
+    
+    paginaActual = paginaNueva;
+} */
